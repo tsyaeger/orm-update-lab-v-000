d540805e8d05dc695e9b6192530f54564848d4f8
taryn yaeger <tsyaeger@gmail.com>
2018-06-16 00:47:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,6 +55,19 @@ class Student
     new_student.save
     new_student
   end
+  
+  
+   def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row| 
+      self.new_from_db(row)
+    end.first
+  end
 
 
 end
