8e0885a372a36d5b4cedf8c22fda56531047952f
taryn yaeger <tsyaeger@gmail.com>
2018-06-16 00:45:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,16 @@ class Student
   end
   
   
+    def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+  
+  
   
 
 
