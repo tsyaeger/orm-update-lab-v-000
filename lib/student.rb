32fc6d04e38a677dc06e8e84462757977d0668f3
taryn yaeger <tsyaeger@gmail.com>
2018-06-16 00:45:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,18 @@ class Student
   end
   
   
+    def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+  
   
 
 
