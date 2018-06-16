705e718ac7fc76897f0afa15a50c391b0a959e52
taryn yaeger <tsyaeger@gmail.com>
2018-06-16 01:04:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,6 @@ class Student
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-    end 
   end  
   
   
