1a7797c4f0ab2dcbed8fa5493ce3fffc872cf430
taryn yaeger <tsyaeger@gmail.com>
2018-06-16 00:48:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,18 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
   
-  
+def save
+  if self.id
+    self.update
+  else
+    sql = <<-SQL
+      INSERT INTO songs (name, album) 
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.album)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end 
+end  
   
   
   
