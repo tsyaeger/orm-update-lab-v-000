cae0fdf3b27288471aebf508a3dee43a98312200
taryn yaeger <tsyaeger@gmail.com>
2018-06-16 01:15:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
 
   attr_accessor :name, :grade, :id
 
-  def initialize(name, grade, id=nil)
+  def initialize(id= nil, name, grade)
     @name = name
     @grade = grade
     @id = id
