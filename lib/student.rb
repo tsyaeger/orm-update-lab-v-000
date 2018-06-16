e81a9fb724e400592044796a487defdc4abee678
taryn yaeger <tsyaeger@gmail.com>
2018-06-16 01:20:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,8 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new
+    puts row
+    new_student = Student.create
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
